f31bfa560fcf787097ed1fd8feac3c2997558678
Yuncong Zhang <yuncong.zhang@unity3d.com>
2019-02-19 15:45:17 +0800

    Code cleanup

--- Runtime/ui/txt/paragraph.cs
+++ Runtime/ui/txt/paragraph.cs
@@ -857,9 +857,9 @@ namespace Unity.UIWidgets.ui {
     }
 
     public class SplayTree<TKey, TValue> : IDictionary<TKey, TValue> where TKey : IComparable<TKey> {
-        private SplayTreeNode root;
-        private int count;
-        private int version = 0;
+        SplayTreeNode root;
+        int count;
+        int version = 0;
 
         public void Add(TKey key, TValue value) {
             this.Set(key, value, throwOnExisting: true);
@@ -869,7 +869,7 @@ namespace Unity.UIWidgets.ui {
             this.Set(item.Key, item.Value, throwOnExisting: true);
         }
 
-        private void Set(TKey key, TValue value, bool throwOnExisting) {
+        void Set(TKey key, TValue value, bool throwOnExisting) {
             if (this.count == 0) {
                 this.version++;
                 this.root = new SplayTreeNode(key, value);
@@ -895,7 +895,8 @@ namespace Unity.UIWidgets.ui {
                 n.LeftChild = this.root.LeftChild;
                 n.RightChild = this.root;
                 this.root.LeftChild = null;
-            } else {
+            }
+            else {
                 n.RightChild = this.root.RightChild;
                 n.LeftChild = this.root;
                 this.root.RightChild = null;
@@ -930,7 +931,9 @@ namespace Unity.UIWidgets.ui {
 
             this.Splay(item.Key);
 
-            return item.Key.CompareTo(this.root.Key) == 0 && (object.ReferenceEquals(this.root.Value, item.Value) || (!object.ReferenceEquals(item.Value, null) && item.Value.Equals(this.root.Value)));
+            return item.Key.CompareTo(this.root.Key) == 0 &&
+                   (ReferenceEquals(this.root.Value, item.Value) ||
+                    (!ReferenceEquals(item.Value, null) && item.Value.Equals(this.root.Value)));
         }
 
         public KeyValuePair<TKey, TValue> First() {
@@ -938,8 +941,11 @@ namespace Unity.UIWidgets.ui {
             if (t == null) {
                 throw new NullReferenceException("The root of this tree is null!");
             }
-            while (t.LeftChild != null)
-            t = t.LeftChild;
+
+            while (t.LeftChild != null) {
+                t = t.LeftChild;
+            }
+
             return new KeyValuePair<TKey, TValue>(t.Key, t.Value);
         }
 
@@ -948,8 +954,11 @@ namespace Unity.UIWidgets.ui {
             if (t == null) {
                 return new KeyValuePair<TKey, TValue>(default(TKey), default(TValue));
             }
-            while (t.LeftChild != null)
+
+            while (t.LeftChild != null) {
                 t = t.LeftChild;
+            }
+
             return new KeyValuePair<TKey, TValue>(t.Key, t.Value);
         }
 
@@ -958,8 +967,11 @@ namespace Unity.UIWidgets.ui {
             if (t == null) {
                 throw new NullReferenceException("The root of this tree is null!");
             }
-            while (t.RightChild != null)
+
+            while (t.RightChild != null) {
                 t = t.RightChild;
+            }
+
             return new KeyValuePair<TKey, TValue>(t.Key, t.Value);
         }
 
@@ -968,45 +980,63 @@ namespace Unity.UIWidgets.ui {
             if (t == null) {
                 return new KeyValuePair<TKey, TValue>(default(TKey), default(TValue));
             }
-            while (t.RightChild != null)
+
+            while (t.RightChild != null) {
                 t = t.RightChild;
+            }
+
             return new KeyValuePair<TKey, TValue>(t.Key, t.Value);
         }
 
-        private void Splay(TKey key) {
+        void Splay(TKey key) {
             SplayTreeNode l, r, t, y, header;
             l = r = header = new SplayTreeNode(default(TKey), default(TValue));
             t = this.root;
             while (true) {
                 var c = key.CompareTo(t.Key);
                 if (c < 0) {
-                    if (t.LeftChild == null) break;
+                    if (t.LeftChild == null) {
+                        break;
+                    }
+
                     if (key.CompareTo(t.LeftChild.Key) < 0) {
                         y = t.LeftChild;
                         t.LeftChild = y.RightChild;
                         y.RightChild = t;
                         t = y;
-                        if (t.LeftChild == null) break;
+                        if (t.LeftChild == null) {
+                            break;
+                        }
                     }
+
                     r.LeftChild = t;
                     r = t;
                     t = t.LeftChild;
-                } else if (c > 0) {
-                    if (t.RightChild == null) break;
+                }
+                else if (c > 0) {
+                    if (t.RightChild == null) {
+                        break;
+                    }
+
                     if (key.CompareTo(t.RightChild.Key) > 0) {
                         y = t.RightChild;
                         t.RightChild = y.LeftChild;
                         y.LeftChild = t;
                         t = y;
-                        if (t.RightChild == null) break;
+                        if (t.RightChild == null) {
+                            break;
+                        }
                     }
+
                     l.RightChild = t;
                     l = t;
                     t = t.RightChild;
-                } else {
+                }
+                else {
                     break;
                 }
             }
+
             l.RightChild = t.LeftChild;
             r.LeftChild = t.RightChild;
             t.LeftChild = header.RightChild;
@@ -1027,7 +1057,8 @@ namespace Unity.UIWidgets.ui {
 
             if (this.root.LeftChild == null) {
                 this.root = this.root.RightChild;
-            } else {
+            }
+            else {
                 var swap = this.root.RightChild;
                 this.root = this.root.LeftChild;
                 this.Splay(key);
@@ -1069,9 +1100,7 @@ namespace Unity.UIWidgets.ui {
                 return this.root.Value;
             }
 
-            set {
-                this.Set(key, value, throwOnExisting: false);
-            }
+            set { this.Set(key, value, throwOnExisting: false); }
         }
 
         public int Count {
@@ -1089,13 +1118,16 @@ namespace Unity.UIWidgets.ui {
 
             this.Splay(item.Key);
 
-            if (item.Key.CompareTo(this.root.Key) == 0 && (object.ReferenceEquals(this.root.Value, item.Value) || (!object.ReferenceEquals(item.Value, null) && item.Value.Equals(this.root.Value)))) {
+            if (item.Key.CompareTo(this.root.Key) == 0 && (ReferenceEquals(this.root.Value, item.Value) ||
+                                                           (!ReferenceEquals(item.Value, null) &&
+                                                            item.Value.Equals(this.root.Value)))) {
                 return false;
             }
 
             if (this.root.LeftChild == null) {
                 this.root = this.root.RightChild;
-            } else {
+            }
+            else {
                 var swap = this.root.RightChild;
                 this.root = this.root.LeftChild;
                 this.Splay(item.Key);
@@ -1118,7 +1150,8 @@ namespace Unity.UIWidgets.ui {
 
             if (depth == 0) {
                 this.Clear();
-            } else {
+            }
+            else {
                 var prevCount = this.count;
                 this.count = this.Trim(this.root, depth - 1);
                 if (prevCount != this.count) {
@@ -1127,20 +1160,21 @@ namespace Unity.UIWidgets.ui {
             }
         }
 
-        private int Trim(SplayTreeNode node, int depth) {
+        int Trim(SplayTreeNode node, int depth) {
             if (depth == 0) {
                 node.LeftChild = null;
                 node.RightChild = null;
                 return 1;
-            } else {
+            }
+            else {
                 int count = 1;
 
                 if (node.LeftChild != null) {
-                    count += Trim(node.LeftChild, depth - 1);
+                    count += this.Trim(node.LeftChild, depth - 1);
                 }
 
                 if (node.RightChild != null) {
-                    count += Trim(node.RightChild, depth - 1);
+                    count += this.Trim(node.RightChild, depth - 1);
                 }
 
                 return count;
@@ -1160,30 +1194,37 @@ namespace Unity.UIWidgets.ui {
         }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
-            return new TiedList<KeyValuePair<TKey, TValue>>(this, this.version, this.AsList(node => new KeyValuePair<TKey, TValue>(node.Key, node.Value))).GetEnumerator();
+            return new TiedList<KeyValuePair<TKey, TValue>>(this, this.version,
+                this.AsList(node => new KeyValuePair<TKey, TValue>(node.Key, node.Value))).GetEnumerator();
         }
 
-        private IList<TEnumerator> AsList<TEnumerator>(Func<SplayTreeNode, TEnumerator> selector) {
+        IList<TEnumerator> AsList<TEnumerator>(Func<SplayTreeNode, TEnumerator> selector) {
             if (this.root == null) {
                 return new TEnumerator[0];
             }
 
             var result = new List<TEnumerator>(this.count);
-            PopulateList(this.root, result, selector);
+            this.PopulateList(this.root, result, selector);
             return result;
         }
 
-        private void PopulateList<TEnumerator>(SplayTreeNode node, List<TEnumerator> list, Func<SplayTreeNode, TEnumerator> selector) {
-            if (node.LeftChild != null) PopulateList(node.LeftChild, list, selector);
+        void PopulateList<TEnumerator>(SplayTreeNode node, List<TEnumerator> list,
+            Func<SplayTreeNode, TEnumerator> selector) {
+            if (node.LeftChild != null) {
+                this.PopulateList(node.LeftChild, list, selector);
+            }
+
             list.Add(selector(node));
-            if (node.RightChild != null) PopulateList(node.RightChild, list, selector);
+            if (node.RightChild != null) {
+                this.PopulateList(node.RightChild, list, selector);
+            }
         }
 
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+        IEnumerator IEnumerable.GetEnumerator() {
             return this.GetEnumerator();
         }
 
-        private sealed class SplayTreeNode {
+        sealed class SplayTreeNode {
             public readonly TKey Key;
 
             public TValue Value;
@@ -1196,10 +1237,10 @@ namespace Unity.UIWidgets.ui {
             }
         }
 
-        private sealed class TiedList<T> : IList<T> {
-            private readonly SplayTree<TKey, TValue> tree;
-            private readonly int version;
-            private readonly IList<T> backingList;
+        sealed class TiedList<T> : IList<T> {
+            readonly SplayTree<TKey, TValue> tree;
+            readonly int version;
+            readonly IList<T> backingList;
 
             public TiedList(SplayTree<TKey, TValue> tree, int version, IList<T> backingList) {
                 if (tree == null) {
@@ -1216,7 +1257,10 @@ namespace Unity.UIWidgets.ui {
             }
 
             public int IndexOf(T item) {
-                if (this.tree.version != this.version) throw new InvalidOperationException("The collection has been modified.");
+                if (this.tree.version != this.version) {
+                    throw new InvalidOperationException("The collection has been modified.");
+                }
+
                 return this.backingList.IndexOf(item);
             }
 
@@ -1230,12 +1274,13 @@ namespace Unity.UIWidgets.ui {
 
             public T this[int index] {
                 get {
-                    if (this.tree.version != this.version) throw new InvalidOperationException("The collection has been modified.");
+                    if (this.tree.version != this.version) {
+                        throw new InvalidOperationException("The collection has been modified.");
+                    }
+
                     return this.backingList[index];
                 }
-                set {
-                    throw new NotSupportedException();
-                }
+                set { throw new NotSupportedException(); }
             }
 
             public void Add(T item) {
@@ -1247,12 +1292,18 @@ namespace Unity.UIWidgets.ui {
             }
 
             public bool Contains(T item) {
-                if (this.tree.version != this.version) throw new InvalidOperationException("The collection has been modified.");
+                if (this.tree.version != this.version) {
+                    throw new InvalidOperationException("The collection has been modified.");
+                }
+
                 return this.backingList.Contains(item);
             }
 
             public void CopyTo(T[] array, int arrayIndex) {
-                if (this.tree.version != this.version) throw new InvalidOperationException("The collection has been modified.");
+                if (this.tree.version != this.version) {
+                    throw new InvalidOperationException("The collection has been modified.");
+                }
+
                 this.backingList.CopyTo(array, arrayIndex);
             }
 
@@ -1269,11 +1320,15 @@ namespace Unity.UIWidgets.ui {
             }
 
             public IEnumerator<T> GetEnumerator() {
-                if (this.tree.version != this.version) throw new InvalidOperationException("The collection has been modified.");
+                if (this.tree.version != this.version) {
+                    throw new InvalidOperationException("The collection has been modified.");
+                }
 
                 foreach (var item in this.backingList) {
                     yield return item;
-                    if (this.tree.version != this.version) throw new InvalidOperationException("The collection has been modified.");
+                    if (this.tree.version != this.version) {
+                        throw new InvalidOperationException("The collection has been modified.");
+                    }
                 }
             }
 
@@ -1282,5 +1337,4 @@ namespace Unity.UIWidgets.ui {
             }
         }
     }
-
 }
