0bc27a5c093e439e0d5c8926213e8dde5372111f
Yuncong Zhang <yuncong.zhang@unity3d.com>
2019-04-10 16:47:13 +0800

    Fix issue.

--- Runtime/engine/global_settings.cs
+++ Runtime/engine/global_settings.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 
 namespace Unity.UIWidgets.engine {
     public class UIWidgetsGlobalSettings {
-        private UIWidgetsGlobalSettings() {}
-        
-        static UIWidgetsGlobalSettings _instance = new UIWidgetsGlobalSettings();
-        Timer scheduleFrameTimer;
-        public const int defaultMaxTargetFrameRate = 60;
-        public const int defaultMinTargetFrameRate = 15;
+        UIWidgetsGlobalSettings() {
+            this.speedUpFrameRate = () => { this.defaultSpeedUp(); };
+            this.coolDownFrameRate = () => { this.defaultCoolDown(); };
+        }
 
+        public Action speedUpFrameRate;
+        public Action coolDownFrameRate;
 
         public static UIWidgetsGlobalSettings instance {
             get { return _instance; }
         }
-
-        public virtual void speedUpFrameRate() {
+        
+        static UIWidgetsGlobalSettings _instance = new UIWidgetsGlobalSettings();
+        
+        Timer scheduleFrameTimer;
+        public const int defaultMaxTargetFrameRate = 60;
+        public const int defaultMinTargetFrameRate = 15;
+        
+        void defaultSpeedUp() {
             Application.targetFrameRate = defaultMaxTargetFrameRate;
         }
 
-        public virtual void coolDownFrameRate() {
+        void defaultCoolDown() {
             this.scheduleFrameTimer?.cancel();
             this.scheduleFrameTimer = Window.instance.run(
                 new TimeSpan(0, 0, 0, 0, 200),
