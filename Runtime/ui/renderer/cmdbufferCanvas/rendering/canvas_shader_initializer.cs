5a0a16937374dee72cc1f663cbdf505fb24e2329
xingwei.zhu <etzhu192@hotmail.com>
2019-09-09 13:02:11 +0800

    fix black screen issue (refine)

--- Runtime/ui/renderer/cmdbufferCanvas/rendering/canvas_shader_initializer.cs
+++ Runtime/ui/renderer/cmdbufferCanvas/rendering/canvas_shader_initializer.cs
@@ -17,9 +17,37 @@ namespace Unity.UIWidgets.ui {
         const string testShaderName = "UIWidgets/canvas_convexFill_cb";
         
         
-        const bool enableComputeBuffer = false;
+        const bool enableComputeBuffer = true;
 
         public static bool supportComputeBuffer;
+
+        static void DoPrepareDefaultShader() {
+            supportComputeBuffer = false;
+            
+            var convexFillShader = GetShader("UIWidgets/canvas_convexFill");
+            var fill0Shader = GetShader("UIWidgets/canvas_fill0");
+            var fill1Shader = GetShader("UIWidgets/canvas_fill1");
+            var stroke0Shader = GetShader("UIWidgets/canvas_stroke0");
+            var stroke1Shader = GetShader("UIWidgets/canvas_stroke1");
+            var texShader = GetShader("UIWidgets/canvas_tex");
+            var stencilShader = GetShader("UIWidgets/canvas_stencil");
+            var filterShader = GetShader("UIWidgets/canvas_filter");
+            var shadowBoxShader = GetShader("UIWidgets/ShadowBox");
+            var shadowRBoxShader = GetShader("UIWidgets/ShadowRBox");
+            var strokeAlphaShader = GetShader("UIWidgets/canvas_strokeAlpha");
+
+            _convexFillMat = new MaterialByBlendModeStencilComp(convexFillShader);
+            _fill0Mat = new MaterialByStencilComp(fill0Shader);
+            _fill1Mat = new MaterialByBlendMode(fill1Shader);
+            _stroke0Mat = new MaterialByBlendModeStencilComp(stroke0Shader);
+            _stroke1Mat = new Material(stroke1Shader) {hideFlags = HideFlags.HideAndDontSave};
+            _strokeAlphaMat = new MaterialByBlendModeStencilComp(strokeAlphaShader);
+            _texMat = new MaterialByBlendModeStencilComp(texShader);
+            _stencilMat = new Material(stencilShader) {hideFlags = HideFlags.HideAndDontSave};
+            _filterMat = new Material(filterShader) {hideFlags = HideFlags.HideAndDontSave};
+            _shadowBox = new Material(shadowBoxShader) {hideFlags = HideFlags.HideAndDontSave};
+            _shadowRBox = new Material(shadowRBoxShader) {hideFlags = HideFlags.HideAndDontSave};
+        }
 
         static bool OnNotPrepared() {
             initStage = InitStage.Prepared;
@@ -52,77 +80,56 @@ namespace Unity.UIWidgets.ui {
             }
 
             ObjectUtils.SafeDestroy(material);
-
-            return false;
+            
+            DoPrepareDefaultShader();
+            return true;
         }
 
         static bool OnPrepared(bool forceReady = false) {
             D.assert(initStage == InitStage.Prepared);
             if (!forceReady && initialFrameCount >= Time.frameCount) {
-                return false;
+                initStage = InitStage.Ready;
+                return true;
             }
 
             initStage = InitStage.Ready;
-            DoPrepare();
+            DoPrepareComputeBufferShader();
             return true;
         }
 
-        static void DoPrepare() {
+        static void DoPrepareComputeBufferShader() {
             D.assert(testShader != null);
             var isShaderSupported = testShader.isSupported;
             testShader = null;
             supportComputeBuffer = enableComputeBuffer && SystemInfo.supportsComputeShaders && isShaderSupported;
 
             if (!supportComputeBuffer) {
-                var convexFillShader = GetShader("UIWidgets/canvas_convexFill");
-                var fill0Shader = GetShader("UIWidgets/canvas_fill0");
-                var fill1Shader = GetShader("UIWidgets/canvas_fill1");
-                var stroke0Shader = GetShader("UIWidgets/canvas_stroke0");
-                var stroke1Shader = GetShader("UIWidgets/canvas_stroke1");
-                var texShader = GetShader("UIWidgets/canvas_tex");
-                var stencilShader = GetShader("UIWidgets/canvas_stencil");
-                var filterShader = GetShader("UIWidgets/canvas_filter");
-                var shadowBoxShader = GetShader("UIWidgets/ShadowBox");
-                var shadowRBoxShader = GetShader("UIWidgets/ShadowRBox");
-                var strokeAlphaShader = GetShader("UIWidgets/canvas_strokeAlpha");
-
-                _convexFillMat = new MaterialByBlendModeStencilComp(convexFillShader);
-                _fill0Mat = new MaterialByStencilComp(fill0Shader);
-                _fill1Mat = new MaterialByBlendMode(fill1Shader);
-                _stroke0Mat = new MaterialByBlendModeStencilComp(stroke0Shader);
-                _stroke1Mat = new Material(stroke1Shader) {hideFlags = HideFlags.HideAndDontSave};
-                _strokeAlphaMat = new MaterialByBlendModeStencilComp(strokeAlphaShader);
-                _texMat = new MaterialByBlendModeStencilComp(texShader);
-                _stencilMat = new Material(stencilShader) {hideFlags = HideFlags.HideAndDontSave};
-                _filterMat = new Material(filterShader) {hideFlags = HideFlags.HideAndDontSave};
-                _shadowBox = new Material(shadowBoxShader) {hideFlags = HideFlags.HideAndDontSave};
-                _shadowRBox = new Material(shadowRBoxShader) {hideFlags = HideFlags.HideAndDontSave};
+                return;
             }
-            else {
-                var convexFillShaderCompute = GetShader("UIWidgets/canvas_convexFill_cb");
-                var fill0ShaderCompute = GetShader("UIWidgets/canvas_fill0_cb");
-                var fill1ShaderCompute = GetShader("UIWidgets/canvas_fill1_cb");
-                var stroke0ShaderCompute = GetShader("UIWidgets/canvas_stroke0_cb");
-                var stroke1ShaderCompute = GetShader("UIWidgets/canvas_stroke1_cb");
-                var texShaderCompute = GetShader("UIWidgets/canvas_tex_cb");
-                var stencilShaderCompute = GetShader("UIWidgets/canvas_stencil_cb");
-                var filterShaderCompute = GetShader("UIWidgets/canvas_filter_cb");
-                var shadowBoxShaderCompute = GetShader("UIWidgets/ShadowBox_cb");
-                var shadowRBoxShaderCompute = GetShader("UIWidgets/ShadowRBox_cb");
-                var strokeAlphaShaderCompute = GetShader("UIWidgets/canvas_strokeAlpha_cb");
 
-                _convexFillMat = new MaterialByBlendModeStencilComp(convexFillShaderCompute);
-                _fill0Mat = new MaterialByStencilComp(fill0ShaderCompute);
-                _fill1Mat = new MaterialByBlendMode(fill1ShaderCompute);
-                _stroke0Mat = new MaterialByBlendModeStencilComp(stroke0ShaderCompute);
-                _stroke1Mat = new Material(stroke1ShaderCompute) {hideFlags = HideFlags.HideAndDontSave};
-                _strokeAlphaMat = new MaterialByBlendModeStencilComp(strokeAlphaShaderCompute);
-                _texMat = new MaterialByBlendModeStencilComp(texShaderCompute);
-                _stencilMat = new Material(stencilShaderCompute) {hideFlags = HideFlags.HideAndDontSave};
-                _filterMat = new Material(filterShaderCompute) {hideFlags = HideFlags.HideAndDontSave};
-                _shadowBox = new Material(shadowBoxShaderCompute) {hideFlags = HideFlags.HideAndDontSave};
-                _shadowRBox = new Material(shadowRBoxShaderCompute) {hideFlags = HideFlags.HideAndDontSave};
-            }
+            var convexFillShaderCompute = GetShader("UIWidgets/canvas_convexFill_cb");
+            var fill0ShaderCompute = GetShader("UIWidgets/canvas_fill0_cb");
+            var fill1ShaderCompute = GetShader("UIWidgets/canvas_fill1_cb");
+            var stroke0ShaderCompute = GetShader("UIWidgets/canvas_stroke0_cb");
+            var stroke1ShaderCompute = GetShader("UIWidgets/canvas_stroke1_cb");
+            var texShaderCompute = GetShader("UIWidgets/canvas_tex_cb");
+            var stencilShaderCompute = GetShader("UIWidgets/canvas_stencil_cb");
+            var filterShaderCompute = GetShader("UIWidgets/canvas_filter_cb");
+            var shadowBoxShaderCompute = GetShader("UIWidgets/ShadowBox_cb");
+            var shadowRBoxShaderCompute = GetShader("UIWidgets/ShadowRBox_cb");
+            var strokeAlphaShaderCompute = GetShader("UIWidgets/canvas_strokeAlpha_cb");
+
+            _convexFillMat = new MaterialByBlendModeStencilComp(convexFillShaderCompute);
+            _fill0Mat = new MaterialByStencilComp(fill0ShaderCompute);
+            _fill1Mat = new MaterialByBlendMode(fill1ShaderCompute);
+            _stroke0Mat = new MaterialByBlendModeStencilComp(stroke0ShaderCompute);
+            _stroke1Mat = new Material(stroke1ShaderCompute) {hideFlags = HideFlags.HideAndDontSave};
+            _strokeAlphaMat = new MaterialByBlendModeStencilComp(strokeAlphaShaderCompute);
+            _texMat = new MaterialByBlendModeStencilComp(texShaderCompute);
+            _stencilMat = new Material(stencilShaderCompute) {hideFlags = HideFlags.HideAndDontSave};
+            _filterMat = new Material(filterShaderCompute) {hideFlags = HideFlags.HideAndDontSave};
+            _shadowBox = new Material(shadowBoxShaderCompute) {hideFlags = HideFlags.HideAndDontSave};
+            _shadowRBox = new Material(shadowRBoxShaderCompute) {hideFlags = HideFlags.HideAndDontSave};
         }
 
         public static bool isReady() {
